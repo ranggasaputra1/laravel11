c3fad687fcfc986414c8f539b156b14ee7bf07a7
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-04-06 09:19:31 +0700

    fix dashboard route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route for first open the app
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route for dashboard pages
 Route::get('/dashboard', function(){
     return view('dashboard');
 });
