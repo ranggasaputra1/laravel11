050961c8f9efa04f7dcc5839a3740b7c1335126c
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2024-04-22 16:37:57 +0700

    create migration for table spaces and SpacePhoto

---
+++ app/Models/SpacePhoto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SpacePhoto extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function space(){
+        return $this->belongsTo(Space::class, 'space_id', 'id');
+    }
+}
